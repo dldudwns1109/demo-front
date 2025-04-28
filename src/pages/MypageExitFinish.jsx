1fe2781f108b47e12f3e831570be7f8b86d9b446
jinyoungTest15 <jinyoung010402@gmail.com>
2025-04-28 15:01:54 +0900

    Feat: "추가 회원 탈퇴 완료 페이지"
    회원 탈퇴 완료 페이지지를 구현했습니다. ✨

--- src/pages/MypageExitFinish.jsx
+++ src/pages/MypageExitFinish.jsx
@@ -1,3 +1,44 @@
+import Header from "../components/Header";
+
 export default function MypageExitFinish() {
-  return <></>;
+  return (
+    <>
+      {/* 헤더 */}
+      <Header/>
+
+      <div className="d-flex flex-column align-items-center" style={{ marginTop:"80px" }}>
+        {/* 회원 탈퇴 제목 */}
+        <h1 style={{ fontSize: "24px", fontWeight: "bold", marginBottom: "24px", color:"#111111" }}>
+          회원 탈퇴가 완료되었습니다
+        </h1>
+        {/* 회원 탈퇴 메세지 */}
+        <h2 style={{ fontSize: "16px", marginBottom: "48px" }}>
+          <span style={{ color:"#666666" }}>DE:MO를 이용해주셔서 진심으로 감사드립니다.</span>
+        </h2>
+        {/* 회원 탈퇴 이미지 */}
+        <div style={{ width: "240px", height:"280px"}}>
+          <img/>
+        </div>
+        {/* 홈이동 버튼 */}
+        <div style={{ width: "360px"}}>
+          <button 
+            style={{
+              width: "100%",
+              padding: "12px",
+              paddingTop: "7px",
+              paddingBottom: "7px",
+              backgroundColor: "#f1f3f5",
+              color: "#6C757D",
+              fontSize: "16px",
+              fontWeight: "bold",
+              border: "none",
+              borderRadius: "8px",
+            }}
+          >
+            홈으로 이동
+          </button>
+        </div>
+      </div>
+    </>
+  );
 }

25894057543fb28fad78e254673e11554e856dba
dldudwns1109 <dldudwns1109@gmail.com>
2025-05-09 18:19:25 +0900

    Feat: "추가 채팅 기능 및 디자인"
    개인 채팅 목록 페이지에서 채팅 기능이 돌아가도록 구현했습니다. ✨ (채팅 db저장과 채팅방 구현은 아직)

--- src/pages/Chat.jsx
+++ src/pages/Chat.jsx
@@ -1,3 +1,258 @@
+import { useCallback, useEffect, useRef, useState } from "react";
+import SockJS from "sockjs-client";
+import { Client } from "@stomp/stompjs";
+import { useRecoilValue } from "recoil";
+import { loginState, userNoState } from "../utils/storage";
+import axios from "axios";
+import moment from "moment";
+import "moment/locale/ko";
+import Header from "../components/Header";
+
+import { FaRegPaperPlane } from "react-icons/fa6";
+
 export default function Chat() {
-  return <></>;
+  const userNo = useRecoilValue(userNoState);
+  const login = useRecoilValue(loginState);
+  const [client, setClient] = useState(null);
+  const [input, setInput] = useState("");
+  const [messages, setMessages] = useState([]);
+
+  const scrollContainerRef = useRef(null);
+
+  const accessToken = axios.defaults.headers.common["Authorization"];
+
+  useEffect(() => {
+    if (!login) return;
+
+    const client = connectToServer();
+    setClient(client);
+
+    return () => {
+      disconnectFromServer(client);
+      setClient(null);
+    };
+  }, [login]);
+
+  useEffect(() => {
+    const scrollContainer = scrollContainerRef.current;
+    if (scrollContainer) {
+      scrollContainer.scrollTop = scrollContainer.scrollHeight;
+    }
+  }, [messages]);
+
+  const connectToServer = useCallback(() => {
+    const socket = new SockJS("http://localhost:8080/ws");
+
+    const client = new Client({
+      webSocketFactory: () => socket,
+      connectHeaders: {
+        accessToken,
+      },
+      onConnect: () => {
+        client.subscribe("/public/member/chat", (message) => {
+          const json = JSON.parse(message.body);
+          json.type = "CHAT";
+          setMessages((prev) => [...prev, json]);
+        });
+      },
+      onDisconnect: () => {},
+      // debug: (str) => console.log(str),
+    });
+
+    client.activate();
+
+    return client;
+  }, []);
+
+  const disconnectFromServer = useCallback((client) => {
+    if (client) client.deactivate();
+  }, []);
+
+  const sendMessageToServer = useCallback(() => {
+    if (
+      client === null ||
+      !client.connected ||
+      !client.active ||
+      !input.trim() ||
+      !login
+    )
+      return;
+
+    const stompMessage = {
+      destination: "/app/member/chat",
+      headers: { accessToken },
+      body: JSON.stringify({ content: input }),
+    };
+
+    client.publish(stompMessage);
+    setInput("");
+  }, [client, input, login]);
+
+  const isLastMessage = useCallback(
+    (message, idx) => {
+      if (idx + 1 === messages.length) return true;
+      if (!idx) return true;
+
+      const nextMessage = messages[idx + 1];
+
+      if (message.accountNo !== nextMessage.accountNo) return true;
+
+      return (
+        moment(message.time).format("YYYY-MM-DD h:mm") !==
+        moment(nextMessage.time).format("YYYY-MM-DD h:mm")
+      );
+    },
+    [messages]
+  );
+
+  const isSenderVisible = useCallback(
+    (message, idx) => {
+      if (userNo === message.accountNo) return false;
+      if (!idx) return true;
+
+      const prevMessage = messages[idx - 1];
+      if (message.accountNo !== prevMessage.accountNo) return true;
+
+      return (
+        moment(message.time).format("YYYY-MM-DD h:mm") !==
+        moment(prevMessage.time).format("YYYY-MM-DD h:mm")
+      );
+    },
+    [messages, userNo]
+  );
+
+  return (
+    <div className="vh-100">
+      <Header loginState={`${login ? "loggined" : "login"}`} input={false} />
+
+      <div style={{ paddingTop: "70px" }}>
+        <div className="mx-auto" style={{ width: "80%" }}>
+          <div
+            ref={scrollContainerRef}
+            className="d-flex flex-column gap-2 overflow-auto p-4"
+            style={{ height: "80vh" }}
+          >
+            {messages.map((message, idx) => (
+              <div
+                key={idx}
+                className={`d-flex ${
+                  message.accountNo === userNo && "justify-content-end"
+                } align-items-end`}
+                style={{ gap: "12px" }}
+              >
+                {message.accountNo === userNo ? (
+                  <>
+                    {isLastMessage(message, idx) && (
+                      <span style={{ fontSize: "14px", color: "#333333" }}>
+                        {moment(message.time).format("a H:mm")}
+                      </span>
+                    )}
+                    <div>
+                      <div
+                        className="py-2"
+                        style={{
+                          backgroundColor: "#F1F3F5",
+                          borderRadius: "8px",
+                          paddingLeft: "12px",
+                          paddingRight: "12px",
+                          maxWidth: "200px",
+                          wordBreak: "break-word",
+                          whiteSpace: "normal",
+                        }}
+                      >
+                        <span style={{ color: "#333333" }}>
+                          {message.content}
+                        </span>
+                      </div>
+                    </div>
+                  </>
+                ) : (
+                  <>
+                    {isSenderVisible(message, idx) && (
+                      <img
+                        className="shadow-sm"
+                        style={{ objectFit: "cover", borderRadius: "999px" }}
+                        src={`http://localhost:8080/api/member/image/${message.accountNo}`}
+                        width={60}
+                        height={60}
+                      />
+                    )}
+                    <div
+                      style={{
+                        paddingLeft: !isSenderVisible(message, idx)
+                          ? "72px"
+                          : "0px",
+                      }}
+                    >
+                      {isSenderVisible(message, idx) && (
+                        <span style={{ fontSize: "14px", color: "#111111" }}>
+                          {message.accountNickname}
+                        </span>
+                      )}
+                      <div
+                        className="py-2"
+                        style={{
+                          backgroundColor: "#F1F3F5",
+                          borderRadius: "8px",
+                          paddingLeft: "12px",
+                          paddingRight: "12px",
+                          maxWidth: "200px",
+                          wordBreak: "break-word",
+                          whiteSpace: "normal",
+                        }}
+                      >
+                        <span style={{ color: "#333333" }}>
+                          {message.content}
+                        </span>
+                      </div>
+                    </div>
+                    {isLastMessage(message, idx) && (
+                      <span style={{ fontSize: "14px", color: "#333333" }}>
+                        {moment(message.time).format("a H:mm")}
+                      </span>
+                    )}
+                  </>
+                )}
+              </div>
+            ))}
+          </div>
+          <div
+            className="d-flex justify-content-between position-absolute"
+            style={{
+              width: "80%",
+              backgroundColor: "#F1F3F5",
+              borderRadius: "8px",
+              bottom: "80px",
+            }}
+          >
+            <input
+              type="text"
+              className="border-0 w-100"
+              style={{
+                backgroundColor: "#F1F3F5",
+                paddingLeft: "12px",
+                paddingTop: "8px",
+                paddingBottom: "8px",
+                borderRadius: "8px",
+                outline: "none",
+              }}
+              placeholder="메세지 입력..."
+              value={input}
+              onChange={(e) => setInput(e.target.value)}
+              onKeyUp={(e) => {
+                if (e.key === "Enter") sendMessageToServer();
+              }}
+            />
+            <button
+              className="btn py-0 pb-1"
+              style={{ height: "40px" }}
+              onClick={sendMessageToServer}
+            >
+              <FaRegPaperPlane size={18} color="#6C757D" />
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
